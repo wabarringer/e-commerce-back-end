0a821111f6e97f84c192b004ce4e969d390cb715
wabarringer <41850199+wabarringer@users.noreply.github.com>
2023-02-20 12:38:19 -0800

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,11 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require("sequelize");
-// import our database connection from config.js
 const sequelize = require("../config/connection");
 const Category = require("./Category");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
-    // define columns
     product_name: {
       type: DataTypes.STRING,
       allowNull: false,
