6438cafe72477f22d526bc59ede6b22965df19fe
wabarringer <41850199+wabarringer@users.noreply.github.com>
2023-02-20 12:39:05 -0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,23 +1,21 @@
-// import models
 const Product = require("./Product");
 const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
   onDelete: "CASCADE",
 });
-// Categories have many Products
+
 Category.hasMany(Product, {
   foreignKey: "category_id",
 });
-// Products belongToMany Tags (through ProductTag)
+
 Product.belongsToMany(Tag, {
   through: ProductTag,
 });
-// Tags belongToMany Products (through ProductTag)
+
 Tag.belongsToMany(Product, {
   through: ProductTag,
 });
